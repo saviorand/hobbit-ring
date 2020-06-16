485ec2d80f4f61751e18613e72c53f25796f1653
Valentine Erokhin <saviorand@gmail.com>
2020-06-16 17:17:15 +0100

    Update postgres-uploaders.js

--- src/lib/postgres-uploaders.js
+++ src/lib/postgres-uploaders.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.DATABASE_URL,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 module.exports = {
   query: (text, params) => pool.query(text, params),
